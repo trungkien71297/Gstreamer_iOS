7e84f598e26fece528cf28a1df98e4ad7f8cbf29
kiennt <kiennt@aoi-sys.vn>
2022-04-28 10:58:20 +0700

    Initial Commit

---
+++ G2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  G2
+//
+//  Created by MAC013 on 4/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
